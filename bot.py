7d05d95a5ad45b7fdff0a88fe29d0989f1e282c2
miyoso <clotilde.bontempspro@gmail.com>
2026-03-17 14:26:22 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,37 @@
 import os
+import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 
 load_dotenv()
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+CHANNEL_ID = os.getenv('DISCORD_CHANNEL_ID')
 
-DISCORD_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-MISP_URL = os.getenv('MISP_URL')
-MISP_KEY = os.getenv('MISP_API_KEY')
+intents = discord.Intents.default()
+intents.message_content = True 
 
-# suite a faire
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f' Bot connecté en tant que : {bot.user.name}')
+    
+  
+    if CHANNEL_ID:
+        channel = bot.get_channel(int_channel_id := int(CHANNEL_ID))
+        if channel:
+            await channel.send("Le bot MISP est en ligne et prêt à surveiller !")
+        else:
+            print(f"Erreur : Impossible de trouver le salon avec l'ID {CHANNEL_ID}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Le bot répond bien.")
+
+if __name__ == "__main__":
+    if TOKEN:
+        bot.run(TOKEN)
+    else:
+        print("Erreur : Aucun TOKEN Discord trouvé dans le fichier .env")
